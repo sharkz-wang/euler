e03aa841808373d34f5e051f25bc263345ddc87d
Sharkz Wang <sharkz.wang@gmail.com>
2015-04-11 18:11:55 +0800

    Problem 003 added to core.clj of Clojure.

--- clojure/src/euler/core.clj
+++ clojure/src/euler/core.clj
@@ -1,6 +1,6 @@
 (ns euler.core
-  (:use [euler.euler_001] [euler.euler_002]))
+  (:use [euler.euler_001] [euler.euler_002] [euler.euler_003]))
 
 (defn -main
   []
-  (println (solve-euler-002)))
+  (println (solve-euler-003)))
